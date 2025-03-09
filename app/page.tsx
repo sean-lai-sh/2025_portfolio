6719fc0db0e8eaa68924bd37887dcf318f4da5fa
Sean Lai <seanlai221@gmail.com>
2025-03-08 21:11:18 -0500

    fixed build err

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 
 import Experience from "@/components/experience";
-import Hero from "@/components/hero";
+import HeroComp from "@/components/heroComp";
 
 
 export default function Home() {
   return (
     <div className="pb-10 px-5 h-full">
-        <Hero/>
+        <HeroComp/>
         <Experience/>
     </div>
   );
